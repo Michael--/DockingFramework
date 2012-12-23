18d5df8a1cff5c19d468f6c2b195849a658c3f8a
Michael Rieck <mail@michaelrieck.de>
2012-12-23 16:21:21 +0100

    Refacturing

--- Docking/Components/ComponentFinder.cs
+++ Docking/Components/ComponentFinder.cs
@@ -8,212 +8,230 @@ using System.Diagnostics;
 
 namespace Docking.Components
 {
-	public class ComponentFinder
-	{
-		public class ComponentFactoryInformation
-		{
-			public ComponentFactoryInformation (String filename, Type[] types, ComponentFactory factory, bool active)
-			{
-				Debug.Assert (factory != null);
-				Filename = filename;
-				Types = types;
-				ComponentFactory = factory;
-				Typename = factory.ToString ();
-				Active = active;
-			}
-			
-			public Widget CreateInstance (DockFrame frame)
-			{
-				Widget widget;
-				try {
-					widget = (Widget)Activator.CreateInstance (ComponentType, new System.Object[] {frame});
-				} catch (Exception e) {
-					Console.WriteLine (e.ToString ());
-					return null;
-				}
-				return widget;
-			}
-			
-			public Type FactoryType	{ get { return ComponentFactory.GetType (); } }
-			public Type ComponentType{ get { return ComponentFactory.TypeOfInstance; } }
-			public String Comment{ get { return ComponentFactory.Comment; } }
-			public String MenuPath{ get { return ComponentFactory.MenuPath; } }
-			public bool IsSingleInstance{ get { return (ComponentFactory.Options & ComponentFactory.Mode.MultipleInstance) != ComponentFactory.Mode.MultipleInstance; } }
-			public bool InstanceMustExist{ get { return (ComponentFactory.Options & ComponentFactory.Mode.AutoCreate) == ComponentFactory.Mode.AutoCreate; } }
-			public bool HideOnCreate { get { return (ComponentFactory.Options & ComponentFactory.Mode.Hidden) == ComponentFactory.Mode.Hidden; } }
-			
-			public String Filename { get; private set; }
-			public Type[] Types { get; private set; }
-			ComponentFactory ComponentFactory { get; set; }
-			public String Typename { get; private set; }
-			public bool Active { get; set; }
-			public Widget DockWidget { get; set; }
-		}
+    public class ComponentFinder
+    {
+        public class ComponentFactoryInformation
+        {
+            public ComponentFactoryInformation (ComponentFactory factory, bool active)
+            {
+                Debug.Assert (factory != null);
+                ComponentFactory = factory;
+                Active = active;
+            }
+            
+            public Widget CreateInstance(DockFrame frame)
+            {
+                Widget widget;
+                try
+                {
+                    widget = (Widget)Activator.CreateInstance (ComponentType, new System.Object[] {frame});
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine (e.ToString ());
+                    return null;
+                }
+                return widget;
+            }
+            
+            public Type FactoryType { get { return ComponentFactory.GetType (); } }
 
-		private List<ComponentFactoryInformation> mComponents = new List<ComponentFactoryInformation> ();
-		
-		public ComponentFactoryInformation[] ComponentInfos{ get { return mComponents.ToArray (); } }
+            public Type ComponentType{ get { return ComponentFactory.TypeOfInstance; } }
 
-		Widget CreateInstance (ComponentFactoryInformation info, DockFrame frame)
-		{
-			Widget widget = info.CreateInstance (frame);
-			
-			// announce types
-			if (widget != null) {
-				ITypes typeInterface = widget as ITypes;
-				if (typeInterface != null) {
-					// collect all interface from all components first before use
-					// avoid double announce of interfaces
-					// TODO: may this loop could be placed at other location
-					Dictionary<Type, Type> knownTypes = new Dictionary<Type, Type> ();
-					foreach (ComponentFactoryInformation i in mComponents) {
-						foreach (Type t in i.Types) {
-							if (!knownTypes.ContainsKey (t))
-								knownTypes.Add (t, t);
-						}
-					}
-					Type[] allTypes = new Type[knownTypes.Count];
-					knownTypes.Values.CopyTo (allTypes, 0);
-					
-					// annouce all interface of all other components in one step
-					typeInterface.Type (allTypes);
-				}
-			}
-			return widget;
-		}
-		
-		public ComponentFactoryInformation FindEntryPoint (Type t)
-		{
-			foreach (ComponentFactoryInformation info in mComponents) {
-				if (t == info.ComponentType)
-					return info;
-			}
-			return null;
-		}
-		
-		private ComponentFactoryInformation FindComponent (String filename, String typename)
-		{
-			foreach (ComponentFactoryInformation info in mComponents) {
-				if (Path.GetFileName (info.Filename) == Path.GetFileName (filename)) {
-					if (info.Typename.Length == 0)
-						return info;
-					if (typename == info.Typename)
-						return info;
-				}
-			}
-			return null;
-		}
+            public String Comment{ get { return ComponentFactory.Comment; } }
 
-		public Widget CreateInstance (Type type, DockFrame frame)
-		{
-			foreach (ComponentFactoryInformation info in mComponents) {
-				Type t = info.ComponentType;
-				if (t != null) {
-					if (t == type) {
-						info.DockWidget = CreateInstance (info, frame);
-						return info.DockWidget;
-					}
-					Type[] myInterfaces = t.FindInterfaces (mTypeFilter, type);
-					if (myInterfaces.Length > 0) {
-						info.DockWidget = CreateInstance (info, frame);
-						return info.DockWidget;
-					}
-				}
-			}
-			return null;
-		}
-		
-		public Widget FindInstance (Type type)
-		{
-			foreach (ComponentFactoryInformation info in mComponents) {
-				Type t = info.ComponentType;
-				if (t != null) {
-					if (t == type) {
-						return info.DockWidget;
-					}
-					Type[] myInterfaces = t.FindInterfaces (mTypeFilter, type);
-					if (myInterfaces.Length > 0 && info.DockWidget != null) {
-						return info.DockWidget;
-					}
-				}
-			}
-			return null;
-		}
+            public String MenuPath{ get { return ComponentFactory.MenuPath; } }
 
-		TypeFilter mTypeFilter = new TypeFilter (InterfaceFilterCallback);
-		private static bool InterfaceFilterCallback (Type typeObj, System.Object criteriaObj)
-		{
-			return typeObj == (Type)criteriaObj;
-		}
-		
-		public void AddComponent (String filename)
-		{
-			try {
-				Assembly asm = Assembly.LoadFrom (filename);
-				Type[] types = asm.GetExportedTypes ();
-				foreach (Type t in types) {
-					if (t.IsClass) {
-						// Add in instance generation
-						Type[] myInterfaces = t.FindInterfaces (mTypeFilter, typeof(IComponentFactory));
-						if (myInterfaces.Length > 0) {
-							try {
-								ComponentFactory cf = (ComponentFactory)Activator.CreateInstance (t);
-								mComponents.Add (new ComponentFactoryInformation (filename, types, cf, true));
-							} catch (InvalidCastException) {
-								// Console.WriteLine("{0}", e.ToString());
-							} catch (MissingMethodException) {
-								// Console.WriteLine("{0}", e.ToString());
-							} catch (ReflectionTypeLoadException) {
-								// Console.WriteLine("{0}", e.ToString());
-							}
-						}
-					}
-				}
-			} catch (TypeLoadException) {
-			} catch (BadImageFormatException) {
-			} catch (ReflectionTypeLoadException) {
-			} catch (FileLoadException) {
-			} catch (MissingMethodException) {
-			}
-		}
+            public bool IsSingleInstance{ get { return (ComponentFactory.Options & ComponentFactory.Mode.MultipleInstance) != ComponentFactory.Mode.MultipleInstance; } }
 
-		public void SearchForComponents (String search)
-		{
-			SearchForComponents (new String[] { search });
-		}
-		
-		public void SearchForComponents (String[]search)
-		{
-			List<String> componentFiles = new List<string> ();
-			foreach (String s in search) {
-				String folder = Path.GetDirectoryName (s);
-				String name = Path.GetFileName (s);
+            public bool InstanceMustExist{ get { return (ComponentFactory.Options & ComponentFactory.Mode.AutoCreate) == ComponentFactory.Mode.AutoCreate; } }
 
-				string[] files = Directory.GetFiles (folder, name);
-				componentFiles.AddRange (files);
-			}
-			foreach (String s in componentFiles)
-				AddComponent (Path.GetFullPath (s));
-		}
-		
-		public void OpenMustExists (DockFrame frame)
-		{
-			foreach (ComponentFactoryInformation info in mComponents) {
-				if (info.InstanceMustExist && info.DockWidget == null) {
-					info.DockWidget = info.CreateInstance (frame);
-					info.DockWidget.Show ();
-					
-					if (info.HideOnCreate)
-						info.DockWidget.Hide ();
-				}
-			}
-		}
-	}
+            public bool HideOnCreate { get { return (ComponentFactory.Options & ComponentFactory.Mode.Hidden) == ComponentFactory.Mode.Hidden; } }
 
-	public interface ITypes
-	{
-		void Type (Type[] types);
-	}
+            ComponentFactory ComponentFactory { get; set; }
 
+            public bool Active { get; set; }
+
+            public Widget DockWidget { get; set; }
+        }
+
+        private List<Type> mTypes = new List<Type>();
+
+        private List<ComponentFactoryInformation> mComponents = new List<ComponentFactoryInformation> ();
+        
+        public ComponentFactoryInformation[] ComponentInfos{ get { return mComponents.ToArray (); } }
+
+        Widget CreateInstance(ComponentFactoryInformation info, DockFrame frame)
+        {
+            Widget widget = info.CreateInstance (frame);
+            return widget;
+        }
+        
+        public ComponentFactoryInformation FindEntryPoint(Type t)
+        {
+            foreach (ComponentFactoryInformation info in mComponents)
+            {
+                if (t == info.ComponentType)
+                    return info;
+            }
+            return null;
+        }
+        
+        private ComponentFactoryInformation FindComponent(String typename)
+        {
+            foreach (ComponentFactoryInformation info in mComponents)
+            {
+                if (typename == info.ComponentType.ToString())
+                    return info;
+            }
+            return null;
+        }
+
+        public Widget CreateInstance(Type type, DockFrame frame)
+        {
+            foreach (ComponentFactoryInformation info in mComponents)
+            {
+                Type t = info.ComponentType;
+                if (t != null)
+                {
+                    if (t == type)
+                    {
+                        info.DockWidget = CreateInstance (info, frame);
+                        return info.DockWidget;
+                    }
+                    Type[] myInterfaces = t.FindInterfaces (mTypeFilter, type);
+                    if (myInterfaces.Length > 0)
+                    {
+                        info.DockWidget = CreateInstance (info, frame);
+                        return info.DockWidget;
+                    }
+                }
+            }
+            return null;
+        }
+        
+        public Widget FindInstance(Type type)
+        {
+            foreach (ComponentFactoryInformation info in mComponents)
+            {
+                Type t = info.ComponentType;
+                if (t != null)
+                {
+                    if (t == type)
+                    {
+                        return info.DockWidget;
+                    }
+                    Type[] myInterfaces = t.FindInterfaces (mTypeFilter, type);
+                    if (myInterfaces.Length > 0 && info.DockWidget != null)
+                    {
+                        return info.DockWidget;
+                    }
+                }
+            }
+            return null;
+        }
+
+        TypeFilter mTypeFilter = new TypeFilter (InterfaceFilterCallback);
+
+        private static bool InterfaceFilterCallback(Type typeObj, System.Object criteriaObj)
+        {
+            return typeObj == (Type)criteriaObj;
+        }
+        
+        void SearchComponents()
+        {
+            foreach (Type t in mTypes)
+            {
+                // find all real classes which implement interface 'IComponentFactory'
+                // and create an inctance of such 
+                if (t.IsClass)
+                {
+                    Type[] myInterfaces = t.FindInterfaces (mTypeFilter, typeof(IComponentFactory));
+                    if (myInterfaces.Length > 0)
+                    {
+                        try
+                        {
+                            ComponentFactory cf = (ComponentFactory)Activator.CreateInstance (t);
+                            mComponents.Add (new ComponentFactoryInformation (cf, true));
+                        }
+                        catch (InvalidCastException)
+                        {
+                            // Console.WriteLine("{0}", e.ToString());
+                        }
+                        catch (MissingMethodException)
+                        {
+                            // Console.WriteLine("{0}", e.ToString());
+                        }
+                        catch (ReflectionTypeLoadException)
+                        {
+                            // Console.WriteLine("{0}", e.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SearchForComponents(String search)
+        {
+            SearchForComponents (new String[] { search });
+        }
+        
+        public void SearchForComponents(String[]search)
+        {
+            List<String> componentFiles = new List<string> ();
+            foreach (String s in search)
+            {
+                String folder = Path.GetDirectoryName (s);
+                String name = Path.GetFileName (s);
+
+                string[] files = Directory.GetFiles (folder, name);
+                componentFiles.AddRange (files);
+            }
+            foreach (String s in componentFiles)
+                CollectTypes (Path.GetFullPath (s));
+
+            SearchComponents();
+        }
+
+        void CollectTypes(String filename)
+        {
+            try
+            {
+                Assembly asm = Assembly.LoadFrom (filename);
+                Type[] types = asm.GetExportedTypes ();
+                mTypes.AddRange(types);
+            }
+            catch (TypeLoadException)
+            {
+            }
+            catch (BadImageFormatException)
+            {
+            }
+            catch (ReflectionTypeLoadException)
+            {
+            }
+            catch (FileLoadException)
+            {
+            }
+            catch (MissingMethodException)
+            {
+            }
+        }
+        
+        public void OpenMustExists(DockFrame frame)
+        {
+            foreach (ComponentFactoryInformation info in mComponents)
+            {
+                if (info.InstanceMustExist && info.DockWidget == null)
+                {
+                    info.DockWidget = info.CreateInstance (frame);
+                    info.DockWidget.Show ();
+                    
+                    if (info.HideOnCreate)
+                        info.DockWidget.Hide ();
+                }
+            }
+        }
+    }
 }
 
