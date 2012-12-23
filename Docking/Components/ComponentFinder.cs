f86643d627e5c8a96ca961be4339bfc610a5bdf5
Michael Rieck <mail@michaelrieck.de>
2012-12-23 15:38:48 +0100

    Refacturing

--- Docking/Components/ComponentFinder.cs
+++ Docking/Components/ComponentFinder.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Docking;
+using Gtk;
+using System.Diagnostics;
 
 namespace Docking.Components
 {
@@ -10,175 +12,89 @@ namespace Docking.Components
 	{
 		public class ComponentFactoryInformation
 		{
-			public ComponentFactoryInformation(String filename, Type[] types, ComponentFactory component, bool active)
+			public ComponentFactoryInformation (String filename, Type[] types, ComponentFactory factory, bool active)
 			{
+				Debug.Assert (factory != null);
 				Filename = filename;
 				Types = types;
-				ComponentFactory = component;
-				Typename = component.ToString();
+				ComponentFactory = factory;
+				Typename = factory.ToString ();
 				Active = active;
 			}
 			
-			public bool Valid
+			public Widget CreateInstance (DockFrame frame)
 			{
-				get
-				{
-					if (ComponentFactory != null)
-						return true;
-					return false;
-				}
-			}
-
-			public Gtk.Bin CreateInstance(DockFrame frame)
-			{
-				Gtk.Bin dc;
-				try
-				{
-					dc = (Gtk.Bin)Activator.CreateInstance(AddInType, new Object[] {frame});
-				}
-				catch(Exception e)
-				{
-					Console.WriteLine(e.ToString());
+				Widget widget;
+				try {
+					widget = (Widget)Activator.CreateInstance (ComponentType, new System.Object[] {frame});
+				} catch (Exception e) {
+					Console.WriteLine (e.ToString ());
 					return null;
 				}
-				return dc;
+				return widget;
 			}
 			
-			public Type FactoryType
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return ComponentFactory.GetType();
-					return null;
-				}
-			}
-			
-			public Type AddInType
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return ComponentFactory.TypeOfInstance;
-					return null;
-				}
-			}
-			
-			public String Comment
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return ComponentFactory.Comment;
-					return null;
-				}
-			}
-			
-			public String MenuPath
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return ComponentFactory.MenuPath;
-					return null;
-				}
-			}
-			
-			public bool IsSingleInstance
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return (ComponentFactory.Options & ComponentFactory.Mode.MultipleInstance) !=ComponentFactory.Mode.MultipleInstance;
-					return true; // should never occur
-				}
-			}
-			
-			public bool InstanceMustExist
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return (ComponentFactory.Options & ComponentFactory.Mode.AutoCreate) == ComponentFactory.Mode.AutoCreate;
-					return false; // should never occur
-				}
-			}
-			
-			public bool HideOnCreate
-			{
-				get
-				{
-					if (ComponentFactory != null)
-						return (ComponentFactory.Options & ComponentFactory.Mode.Hidden) == ComponentFactory.Mode.Hidden;
-					return false; // should never occur
-				}
-			}
+			public Type FactoryType	{ get { return ComponentFactory.GetType (); } }
+			public Type ComponentType{ get { return ComponentFactory.TypeOfInstance; } }
+			public String Comment{ get { return ComponentFactory.Comment; } }
+			public String MenuPath{ get { return ComponentFactory.MenuPath; } }
+			public bool IsSingleInstance{ get { return (ComponentFactory.Options & ComponentFactory.Mode.MultipleInstance) != ComponentFactory.Mode.MultipleInstance; } }
+			public bool InstanceMustExist{ get { return (ComponentFactory.Options & ComponentFactory.Mode.AutoCreate) == ComponentFactory.Mode.AutoCreate; } }
+			public bool HideOnCreate { get { return (ComponentFactory.Options & ComponentFactory.Mode.Hidden) == ComponentFactory.Mode.Hidden; } }
 			
 			public String Filename { get; private set; }
 			public Type[] Types { get; private set; }
 			ComponentFactory ComponentFactory { get; set; }
-			
-			
 			public String Typename { get; private set; }
 			public bool Active { get; set; }
-			public Gtk.Bin DockWidget { get; set; }
+			public Widget DockWidget { get; set; }
 		}
 
-		private List<ComponentFactoryInformation> mComponents = new List<ComponentFactoryInformation>();
+		private List<ComponentFactoryInformation> mComponents = new List<ComponentFactoryInformation> ();
 		
-		public ComponentFactoryInformation[] ComponentInfos
-		{
-			get { return mComponents.ToArray(); }
-		}
+		public ComponentFactoryInformation[] ComponentInfos{ get { return mComponents.ToArray (); } }
 
-		Gtk.Bin CreateInstance(ComponentFactoryInformation info, DockFrame frame)
+		Widget CreateInstance (ComponentFactoryInformation info, DockFrame frame)
 		{
-			Gtk.Bin dc = info.CreateInstance(frame);
+			Widget widget = info.CreateInstance (frame);
 			
 			// announce types
-			if (dc != null)
-			{
-				ITypes typeInterface = dc as ITypes;
-				if (typeInterface != null)
-				{
+			if (widget != null) {
+				ITypes typeInterface = widget as ITypes;
+				if (typeInterface != null) {
 					// collect all interface from all components first before use
 					// avoid double announce of interfaces
 					// TODO: may this loop could be placed at other location
-					Dictionary<Type, Type> knownTypes = new Dictionary<Type, Type>();
-					foreach (ComponentFactoryInformation i in mComponents)
-					{
-						foreach(Type t in i.Types)
-						{
-							if (!knownTypes.ContainsKey(t))
-								knownTypes.Add(t, t);
+					Dictionary<Type, Type> knownTypes = new Dictionary<Type, Type> ();
+					foreach (ComponentFactoryInformation i in mComponents) {
+						foreach (Type t in i.Types) {
+							if (!knownTypes.ContainsKey (t))
+								knownTypes.Add (t, t);
 						}
 					}
 					Type[] allTypes = new Type[knownTypes.Count];
-					knownTypes.Values.CopyTo(allTypes, 0);
+					knownTypes.Values.CopyTo (allTypes, 0);
 					
 					// annouce all interface of all other components in one step
-					typeInterface.Type(allTypes);
+					typeInterface.Type (allTypes);
 				}
 			}
-			return dc;
+			return widget;
 		}
 		
-		public ComponentFactoryInformation FindEntryPoint(Type t)
+		public ComponentFactoryInformation FindEntryPoint (Type t)
 		{
-			foreach (ComponentFactoryInformation info in mComponents)
-			{
-				if (t == info.AddInType)
+			foreach (ComponentFactoryInformation info in mComponents) {
+				if (t == info.ComponentType)
 					return info;
 			}
 			return null;
 		}
 		
-		private ComponentFactoryInformation FindComponent(String filename, String typename)
+		private ComponentFactoryInformation FindComponent (String filename, String typename)
 		{
-			foreach (ComponentFactoryInformation info in mComponents)
-			{
-				if (Path.GetFileName(info.Filename) == Path.GetFileName(filename))
-				{
+			foreach (ComponentFactoryInformation info in mComponents) {
+				if (Path.GetFileName (info.Filename) == Path.GetFileName (filename)) {
 					if (info.Typename.Length == 0)
 						return info;
 					if (typename == info.Typename)
@@ -188,22 +104,18 @@ namespace Docking.Components
 			return null;
 		}
 
-		public Gtk.Bin CreateInstance(Type type, DockFrame frame)
+		public Widget CreateInstance (Type type, DockFrame frame)
 		{
-			foreach (ComponentFactoryInformation info in mComponents)
-			{
-				Type t = info.AddInType;
-				if (t != null)
-				{
-					if(t==type)
-					{
-						info.DockWidget = CreateInstance(info, frame);
+			foreach (ComponentFactoryInformation info in mComponents) {
+				Type t = info.ComponentType;
+				if (t != null) {
+					if (t == type) {
+						info.DockWidget = CreateInstance (info, frame);
 						return info.DockWidget;
 					}
-					Type[] myInterfaces = t.FindInterfaces(mTypeFilter, type);
-					if (myInterfaces.Length > 0)
-					{
-						info.DockWidget = CreateInstance(info, frame);
+					Type[] myInterfaces = t.FindInterfaces (mTypeFilter, type);
+					if (myInterfaces.Length > 0) {
+						info.DockWidget = CreateInstance (info, frame);
 						return info.DockWidget;
 					}
 				}
@@ -211,20 +123,16 @@ namespace Docking.Components
 			return null;
 		}
 		
-		public Gtk.Bin FindInstance(Type type)
+		public Widget FindInstance (Type type)
 		{
-			foreach (ComponentFactoryInformation info in mComponents)
-			{
-				Type t = info.AddInType;
-				if (t != null)
-				{
-					if (t == type)
-					{
+			foreach (ComponentFactoryInformation info in mComponents) {
+				Type t = info.ComponentType;
+				if (t != null) {
+					if (t == type) {
 						return info.DockWidget;
 					}
-					Type[] myInterfaces = t.FindInterfaces(mTypeFilter, type);
-					if(myInterfaces.Length>0 && info.DockWidget!=null)
-					{
+					Type[] myInterfaces = t.FindInterfaces (mTypeFilter, type);
+					if (myInterfaces.Length > 0 && info.DockWidget != null) {
 						return info.DockWidget;
 					}
 				}
@@ -232,74 +140,52 @@ namespace Docking.Components
 			return null;
 		}
 
-		TypeFilter mTypeFilter = new TypeFilter(InterfaceFilterCallback);
-		private static bool InterfaceFilterCallback(Type typeObj, Object criteriaObj)
+		TypeFilter mTypeFilter = new TypeFilter (InterfaceFilterCallback);
+		private static bool InterfaceFilterCallback (Type typeObj, System.Object criteriaObj)
 		{
 			return typeObj == (Type)criteriaObj;
 		}
 		
-		public void AddComponent(String filename)
+		public void AddComponent (String filename)
 		{
-			try
-			{
-				Assembly asm = Assembly.LoadFrom(filename);
-				Type[] types = asm.GetExportedTypes();
-				foreach (Type t in types)
-				{
-					if (t.IsClass)
-					{
+			try {
+				Assembly asm = Assembly.LoadFrom (filename);
+				Type[] types = asm.GetExportedTypes ();
+				foreach (Type t in types) {
+					if (t.IsClass) {
 						// Add in instance generation
-						Type[] myInterfaces = t.FindInterfaces(mTypeFilter, typeof(IComponentFactory));
-						if (myInterfaces.Length > 0)
-						{
-							try
-							{
-								ComponentFactory cf = (ComponentFactory)Activator.CreateInstance(t);
-								mComponents.Add(new ComponentFactoryInformation(filename, types, cf, true));
-							}
-							catch (InvalidCastException)
-							{
+						Type[] myInterfaces = t.FindInterfaces (mTypeFilter, typeof(IComponentFactory));
+						if (myInterfaces.Length > 0) {
+							try {
+								ComponentFactory cf = (ComponentFactory)Activator.CreateInstance (t);
+								mComponents.Add (new ComponentFactoryInformation (filename, types, cf, true));
+							} catch (InvalidCastException) {
 								// Console.WriteLine("{0}", e.ToString());
-							}
-							catch (MissingMethodException)
-							{
+							} catch (MissingMethodException) {
 								// Console.WriteLine("{0}", e.ToString());
-							}
-							catch (ReflectionTypeLoadException)
-							{
+							} catch (ReflectionTypeLoadException) {
 								// Console.WriteLine("{0}", e.ToString());
 							}
 						}
 					}
 				}
-			}
-			catch (TypeLoadException)
-			{
-			}
-			catch (BadImageFormatException)
-			{
-			}
-			catch (ReflectionTypeLoadException)
-			{
-			}
-			catch (FileLoadException)
-			{
-			}
-			catch (MissingMethodException)
-			{
+			} catch (TypeLoadException) {
+			} catch (BadImageFormatException) {
+			} catch (ReflectionTypeLoadException) {
+			} catch (FileLoadException) {
+			} catch (MissingMethodException) {
 			}
 		}
 
-		public void SearchForComponents(String search)
+		public void SearchForComponents (String search)
 		{
-			SearchForComponents(new String[] { search });
+			SearchForComponents (new String[] { search });
 		}
 		
 		public void SearchForComponents (String[]search)
 		{
 			List<String> componentFiles = new List<string> ();
-			foreach (String s in search)
-			{
+			foreach (String s in search) {
 				String folder = Path.GetDirectoryName (s);
 				String name = Path.GetFileName (s);
 
@@ -307,20 +193,18 @@ namespace Docking.Components
 				componentFiles.AddRange (files);
 			}
 			foreach (String s in componentFiles)
-				AddComponent(Path.GetFullPath(s));
+				AddComponent (Path.GetFullPath (s));
 		}
 		
-		public void OpenMustExists(DockFrame frame)
+		public void OpenMustExists (DockFrame frame)
 		{
-			foreach (ComponentFactoryInformation info in mComponents)
-			{
-				if (info.InstanceMustExist && info.DockWidget == null)
-				{
-					info.DockWidget = info.CreateInstance(frame);
-					info.DockWidget.Show();
+			foreach (ComponentFactoryInformation info in mComponents) {
+				if (info.InstanceMustExist && info.DockWidget == null) {
+					info.DockWidget = info.CreateInstance (frame);
+					info.DockWidget.Show ();
 					
 					if (info.HideOnCreate)
-						info.DockWidget.Hide();
+						info.DockWidget.Hide ();
 				}
 			}
 		}
@@ -328,7 +212,7 @@ namespace Docking.Components
 
 	public interface ITypes
 	{
-		void Type(Type[] types);
+		void Type (Type[] types);
 	}
 
 }
