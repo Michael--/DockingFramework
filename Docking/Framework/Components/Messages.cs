d800bc0cf5a255cadf8d50b6ca7de5893a20ed7c
S.Lohse <steffen.lohse@neusoft.com>
2013-03-04 11:42:26 +0100

    renamed Message-16.png to Messages-16.png to make its filename match Messages.cs

--- Docking/Framework/Components/Messages.cs
+++ Docking/Framework/Components/Messages.cs
@@ -37,11 +37,11 @@ namespace Docking.Components
 
         #region implement IComponent
         public ComponentManager ComponentManager { get; set; }
-        
+
         void IComponent.Loaded(DockItem item)
         {
         }
-        
+
         void IComponent.Save()
         {
         }
@@ -62,7 +62,7 @@ namespace Docking.Components
         public override Type TypeOfInstance { get { return typeof(Messages); } }
         public override String MenuPath { get { return @"View\Infrastructure\Messages"; } }
         public override String Comment { get { return "shows runtime messages of the application"; } }
-        public override Gdk.Pixbuf Icon { get { return Gdk.Pixbuf.LoadFromResource ("Docking.Framework.Components.Message-16.png"); } }
+        public override Gdk.Pixbuf Icon { get { return Gdk.Pixbuf.LoadFromResource ("Docking.Framework.Components.Messages-16.png"); } }
     }
 
     #endregion
