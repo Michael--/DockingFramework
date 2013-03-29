374cff6c9aa35d605df10bb57a355f36cb3248a5
Michael Rieck <mail@michaelrieck.de>
2013-03-29 16:28:04 +0100

    Add prime calc python sample

--- DockingExamples/Python/Python.cs
+++ DockingExamples/Python/Python.cs
@@ -81,6 +81,7 @@ namespace Examples
 
             combo.AppendText("test1.py");
             combo.AppendText("test2.py");
+            combo.AppendText("test3.py");
             TreeIter it;
             combo.Model.GetIterFirst(out it);
             combo.SetActiveIter(it);
