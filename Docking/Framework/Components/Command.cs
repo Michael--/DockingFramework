1d850364e43955f2e50ae3451c5315f05afcfc28
M.Rieck <michael.rieck@neusoft.com>
2013-04-23 08:43:37 +0200

    Command load persistence in a thread.
    Unfortunately current persistence methods are slowed down by XML serializer creation.

--- Docking/Framework/Components/Command.cs
+++ Docking/Framework/Components/Command.cs
@@ -3,84 +3,88 @@ using System.Text;
 using MonoDevelop.Components;
 using System.Threading;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Docking.Components
 {
-    [System.ComponentModel.ToolboxItem(false)]
-    public partial class Command : Gtk.Bin, IComponent, IComponentInteract
-    {
-        #region implement IComponent
-        public ComponentManager ComponentManager { get; set; }
-        
-        void IComponent.Loaded(DockItem item)
-        {
-            mPersistence = (CommandPersistence)ComponentManager.LoadObject("Command", typeof(CommandPersistence));
+   [System.ComponentModel.ToolboxItem(false)]
+   public partial class Command : Gtk.Bin, IComponent, IComponentInteract
+   {
+      #region implement IComponent
+      public ComponentManager ComponentManager { get; set; }
+
+      void IComponent.Loaded(DockItem item)
+      {
+         mPersistence = (CommandPersistence)ComponentManager.LoadObject("Command", typeof(CommandPersistence), item);
+
+         Task t = Task.Factory.StartNew(() =>
+         {
             if (mPersistence == null)
-                mPersistence = new CommandPersistence() { Script = "" }; // TODO: set a default script here
+               mPersistence = new CommandPersistence() { Script = "" }; // TODO: set a default script here
 
             try
             {
-                ComponentManager.Execute(mPersistence.Script);
+               ComponentManager.Execute(mPersistence.Script);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                consoleview.WriteOutput("Error: " + ex.Message);
-                consoleview.Prompt(true);
+               consoleview.WriteOutput("Error: " + ex.Message);
+               consoleview.Prompt(true);
             }
 
             // redirect print message and access to this using "command"
             command = new _Command(this, consoleview, ComponentManager);
             ComponentManager.ScriptScope.SetVariable("cmd", command);
             ComponentManager.Execute(String.Join("\r\n", pyPrint));
-        }
+         });
+      }
+      CommandPersistence mPersistence;
 
-        CommandPersistence mPersistence;
+      void IComponent.Save()
+      {
+         ComponentManager.SaveObject("Command", mPersistence);
+      }
 
-        void IComponent.Save()
-        {
-            ComponentManager.SaveObject("Command", mPersistence);
-        }
-        
-        #endregion
+      #endregion
 
-        #region implement IComponentInteract
-        
-        List<IScript> m_ScriptInterface = new List<IScript>();
-        void IComponentInteract.Added(object item)
-        {
-            if (item is IScript)
-            {
-                IScript script = item as IScript;
-                m_ScriptInterface.Add(script);
+      #region implement IComponentInteract
 
-                script.ScriptChanged += ScriptChanged;
-                
-            }
-        }
+      List<IScript> m_ScriptInterface = new List<IScript>();
+      void IComponentInteract.Added(object item)
+      {
+         if (item is IScript)
+         {
+            IScript script = item as IScript;
+            m_ScriptInterface.Add(script);
 
-        void IComponentInteract.Removed(object item)
-        {
-            if (item is IScript)
-                m_ScriptInterface.Remove(item as IScript);
-        }
-        
-        void IComponentInteract.Visible(object item, bool visible)
-        {
-        }
-        
-        void IComponentInteract.Current(object item)
-        {
-            if (this == item)
-            {
-                foreach(IScript it in m_ScriptInterface)
-                    it.SetScript(this, mPersistence.Script);
-            }
-        }
-        
-        #endregion
+            script.ScriptChanged += ScriptChanged;
 
-        #region Python command extensions e.g. print
-        string []pyPrint = new string[] 
+         }
+      }
+
+      void IComponentInteract.Removed(object item)
+      {
+         if (item is IScript)
+            m_ScriptInterface.Remove(item as IScript);
+      }
+
+      void IComponentInteract.Visible(object item, bool visible)
+      {
+      }
+
+      void IComponentInteract.Current(object item)
+      {
+         if (this == item)
+         {
+            foreach (IScript it in m_ScriptInterface)
+               it.SetScript(this, mPersistence.Script);
+         }
+      }
+
+      #endregion
+
+      #region Python command extensions e.g. print
+      string[] pyPrint = new string[] 
         { 
             "#output can be redirected to any object which implement method write and property softspace",
             "import sys",
@@ -88,121 +92,121 @@ namespace Docking.Components
             "sys.stdout=cmd"
         };
 
-        _Command command;
-        public bool DisableInvoke { get; set; }
+      _Command command;
+      public bool DisableInvoke { get; set; }
 
-        // encapsulate python access to c#, reduce access to well known methods
-        public class _Command
-        {
-            public _Command(Command cmd, ConsoleView cv, ComponentManager cm)
+      // encapsulate python access to c#, reduce access to well known methods
+      public class _Command
+      {
+         public _Command(Command cmd, ConsoleView cv, ComponentManager cm)
+         {
+            Command = cmd;
+            ConsoleView = cv;
+            ComponentManager = cm;
+         }
+
+         private Command Command { get; set; }
+         private ConsoleView ConsoleView { get; set; }
+         private ComponentManager ComponentManager { get; set; }
+
+         public void write(string s)
+         {
+            if (Command.DisableInvoke)
             {
-                Command = cmd;
-                ConsoleView = cv;
-                ComponentManager = cm;
-            }
-
-            private Command Command { get; set; }
-            private ConsoleView ConsoleView { get; set; }
-            private ComponentManager ComponentManager { get; set; }
-
-            public void write(string s)
-            {
-                if (Command.DisableInvoke)
-                {
-                    ConsoleView.WriteOutput(s);
-                }
-                else
-                {
-                    Gtk.Application.Invoke(delegate
-                    {
-                        ConsoleView.WriteOutput(s);
-                    });
-                }
-            }
-            
-            public int softspace { get; set; }
-            
-            /// <summary>
-            /// exit application
-            /// </summary>
-            public void quit()
-            {
-                ComponentManager.quit();
-            }
-        }
-
-        #endregion
-
-        #region MAIN
-        public Command()
-        {
-            this.Build();
-            this.Name = "Command";
-            consoleview.ConsoleInput += HandleConsoleInput;
-        }
-
-        void ScriptChanged(ScriptChangedEventArgs e)
-        {
-            if (e.Reference == this)
-            {
-                mPersistence.Script = e.Script;
-                try
-                {
-                    if (e.Code != null)
-                        ComponentManager.Execute(e.Code);
-                }
-                catch(Exception ex)
-                {
-                    foreach(IScript i in m_ScriptInterface)
-                        i.SetMessage(this, ex.Message);
-                }
-            }
-        }
-
-        void HandleConsoleInput (object sender, MonoDevelop.Components.ConsoleInputEventArgs e)
-        {
-            string input = e.Text;
-            if (input != null)
-            {
-                try
-                {
-                    DisableInvoke = true;
-                    ComponentManager.Execute(input);
-                    DisableInvoke = true;
-                    consoleview.Prompt(false);
-                }
-                catch (Exception ex)
-                {
-                    consoleview.WriteOutput("Error: " + ex.Message);
-                    consoleview.Prompt(true);
-                }
+               ConsoleView.WriteOutput(s);
             }
             else
             {
-                consoleview.Prompt(false);
+               Gtk.Application.Invoke(delegate
+               {
+                  ConsoleView.WriteOutput(s);
+               });
             }
-        }
-        #endregion
-    }
+         }
 
-    public class CommandPersistence
-    {
-        public string Script { get; set; }
-    }
+         public int softspace { get; set; }
+
+         /// <summary>
+         /// exit application
+         /// </summary>
+         public void quit()
+         {
+            ComponentManager.quit();
+         }
+      }
+
+      #endregion
+
+      #region MAIN
+      public Command()
+      {
+         this.Build();
+         this.Name = "Command";
+         consoleview.ConsoleInput += HandleConsoleInput;
+      }
+
+      void ScriptChanged(ScriptChangedEventArgs e)
+      {
+         if (e.Reference == this)
+         {
+            mPersistence.Script = e.Script;
+            try
+            {
+               if (e.Code != null)
+                  ComponentManager.Execute(e.Code);
+            }
+            catch (Exception ex)
+            {
+               foreach (IScript i in m_ScriptInterface)
+                  i.SetMessage(this, ex.Message);
+            }
+         }
+      }
+
+      void HandleConsoleInput(object sender, MonoDevelop.Components.ConsoleInputEventArgs e)
+      {
+         string input = e.Text;
+         if (input != null)
+         {
+            try
+            {
+               DisableInvoke = true;
+               ComponentManager.Execute(input);
+               DisableInvoke = true;
+               consoleview.Prompt(false);
+            }
+            catch (Exception ex)
+            {
+               consoleview.WriteOutput("Error: " + ex.Message);
+               consoleview.Prompt(true);
+            }
+         }
+         else
+         {
+            consoleview.Prompt(false);
+         }
+      }
+      #endregion
+   }
+
+   public class CommandPersistence
+   {
+      public string Script { get; set; }
+   }
 
 
 
-    #region Starter / Entry Point
-    
-    public class CommandFactory : ComponentFactory
-    {
-        public override Type TypeOfInstance { get { return typeof(Command); } }
-        public override String MenuPath { get { return @"View\Infrastructure\Command"; } }
-        public override String Comment { get { return "Command line"; } }
-        public override Gdk.Pixbuf Icon { get { return Gdk.Pixbuf.LoadFromResource ("Docking.Framework.Components.Messages-16.png"); } }
-    }
-    
-    #endregion
+   #region Starter / Entry Point
+
+   public class CommandFactory : ComponentFactory
+   {
+      public override Type TypeOfInstance { get { return typeof(Command); } }
+      public override String MenuPath { get { return @"View\Infrastructure\Command"; } }
+      public override String Comment { get { return "Command line"; } }
+      public override Gdk.Pixbuf Icon { get { return Gdk.Pixbuf.LoadFromResource("Docking.Framework.Components.Messages-16.png"); } }
+   }
+
+   #endregion
 
 }
 
