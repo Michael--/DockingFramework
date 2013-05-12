935e214dc228e37da08ed96eac5e20483b2b8664
Michael Rieck <mail@michaelrieck.de>
2013-05-12 19:14:01 +0200

    Moved to project DockingFrameworkTest

--- DockingExamples/Python/test1.py
+++
@@ -1,4 +0,0 @@
-# simpliest python program, print a message on console
-# therefore you can't see any effect when executing except in debugger 
-
-print 'Hello Python World'
--- DockingExamples/Python/test4.py
+++
@@ -1,7 +0,0 @@
-# python can use C# objects
-# use a global object passed from C# to open a text file
-
-result = cm.OpenFile("Python/dummy.txt")
-cm.MessageWriteLine("Python open file returned: " + str(result));
-
-
