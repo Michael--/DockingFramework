92453e6ad1dc3b1285f33a5ad7b0f4bc13a8a1f8
S.Lohse <steffen.lohse@neusoft.com>
2013-08-26 10:49:40 +0200

    added cut/copy/paste interfaces

---
+++ Docking/Framework/Interfaces/ICutCopyPaste.cs
@@ -0,0 +1,22 @@
+﻿namespace Docking.Components
+{
+   // a class implementing this interface supports cutting its current selection into the clipboard
+   public interface ICut
+   {
+      void Cut();
+   }
+
+   // a class implementing this interface supports copying its current selection to the clipboard
+   public interface ICopy
+   {
+      void Copy();
+   }
+
+   // a class implementing this interface supports pasting the current clipboard contents into its current selection
+   public interface IPaste
+   {
+      void Paste();
+   }
+}
+
+   
