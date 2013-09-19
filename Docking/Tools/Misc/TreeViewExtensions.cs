aebc8ec33cf45fa36f695d2ced11950a380435e1
S.Lohse <steffen.lohse@neusoft.com>
2013-09-19 12:39:27 +0200

    relocated function ShowPopupMenu() from Positioning/Result.cs to our TreeViewExtensions collection

--- Docking/Tools/Misc/TreeViewExtensions.cs
+++ Docking/Tools/Misc/TreeViewExtensions.cs
@@ -1,11 +1,13 @@
-﻿using Docking.Components;
-using System;
+﻿using System;
+using Docking.Components;
+using Gtk;
+
 
 namespace Docking.Tools
 {
    public static class TreeViewExtensions
    {
-      static public TreeViewColumnLocalized AppendColumn(this Gtk.TreeView treeview, TreeViewColumnLocalized column, Gtk.CellRenderer renderer, string attr, int modelcolumn)
+      static public TreeViewColumnLocalized AppendColumn(this TreeView treeview, TreeViewColumnLocalized column, CellRenderer renderer, string attr, int modelcolumn)
       {
          treeview.AppendColumn(column);
          column.PackStart(renderer, true);
@@ -16,16 +18,16 @@ namespace Docking.Tools
       // This function is a workaround for the problem that you cannot write
       //    column.Width = width;
       // because .Width (strangely) is a READONLY property.
-      static public void SetWidth(this Gtk.TreeViewColumn column, int width)
+      static public void SetWidth(this TreeViewColumn column, int width)
       {
          if(width<0)
             width = 0;
          
          // saving the old sizing mode and restoring it strangely does not work:
-         // Gtk.TreeViewColumnSizing sizing = column.Sizing;
+         // TreeViewColumnSizing sizing = column.Sizing;
          // instead, we at least properly restore the .Resizable property...:
          bool resizable = column.Resizable;
-         column.Sizing = Gtk.TreeViewColumnSizing.Fixed;
+         column.Sizing = TreeViewColumnSizing.Fixed;
 
          column.FixedWidth = width;
          if(column.MaxWidth>=0 && column.MaxWidth<width)
@@ -38,5 +40,28 @@ namespace Docking.Tools
          //column.Sizing = sizing; // does not work, will throw away our width settings again >:(
          column.Resizable = resizable;
       }
+
+      static public void ShowContextMenu(this TreeView treeview, Menu menu, bool add_menu_entries_for_column_visibility, uint time)
+      {
+         if(add_menu_entries_for_column_visibility)
+         {
+            foreach (TreeViewColumn c in treeview.Columns)
+            {
+               TaggedLocalizedCheckedMenuItem item = new TaggedLocalizedCheckedMenuItem(c.Title);
+               item.Active = c.Visible;
+               item.Tag = c;
+               item.Activated += (object sender, EventArgs e) =>
+               {
+                  TaggedLocalizedCheckedMenuItem it = sender as TaggedLocalizedCheckedMenuItem;
+                  TreeViewColumn ct = it.Tag as TreeViewColumn;
+                  ct.Visible = !ct.Visible;
+               };
+               menu.Add(item);
+            }
+         }
+         menu.ShowAll();
+         menu.Popup(null, null, null, 3, time);
+      }
+
    }
 }
