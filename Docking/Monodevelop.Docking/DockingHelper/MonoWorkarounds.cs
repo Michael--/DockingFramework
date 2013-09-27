810bb005dbc4ab9f00f8fec556bad949f173a809
S.Lohse <steffen.lohse@neusoft.com>
2013-09-27 17:26:52 +0200

    implemented 2 of 4 missing functions for Linux

--- Docking/Monodevelop.Docking/DockingHelper/MonoWorkarounds.cs
+++ Docking/Monodevelop.Docking/DockingHelper/MonoWorkarounds.cs
@@ -5,28 +5,30 @@ using Cairo;
 
 namespace Docking
 {
-	public static class MonoWorkarounds
-	{
-		public static void SetSource(this Context context, Surface surface)
-        {
-           throw new Exception("implement me");
-        }
+   public static class MonoWorkarounds
+   {
+      public static void SetSource(this Context context, Surface surface)
+      {
+         context.SetSourceSurface(surface, 0, 0);
+      }
 
-        public static Surface GetTarget(this Context context)
-        {
-           throw new Exception("implement me");
-        }
+      public static Surface GetTarget(this Context context)
+      {
+         return context.Target;
+      }
 
-		public static void SetSource(this Context context, Pattern pattern)
-        {
-           throw new Exception("implement me");           
-        }
+      public static void SetSource(this Context context, Pattern pattern)
+      {
+         throw new Exception("implement me");
+      }
 
-        public static void Dispose(this Context context)
-        {
-           throw new Exception("implement me");           
-        }
-	}
+      public static void Dispose(this Context context)
+      {
+         //(context as IDisposable).Dispose(true);
+         //GC.SuppressFinalize(context);
+         throw new Exception("implement me");
+      }
+   }
 }
 
 #endif
