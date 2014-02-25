b58fc238b4e3572b4fafe244f8c1aad45486cd6f
S.Lohse <steffen.lohse@neusoft.com>
2014-02-25 12:21:16 +0100

    added more MessageBox.Show() convenience functions

--- Docking/Framework/Components/MessageBox.cs
+++ Docking/Framework/Components/MessageBox.cs
@@ -5,27 +5,43 @@ namespace Docking.Components
 {
     public class MessageBox
     {            
-		public static ResponseType Show(Window parent, MessageType mt, ButtonsType bt, string format, params object[] args)
+		public static ResponseType Show(Window parent, MessageType msgtype, ButtonsType buttontype, string format, params object[] args)
 		{
-			MessageDialog md = new MessageDialog (parent, 
-			                                      DialogFlags.Modal,
-			                                      mt, 
-			                                      bt,
-			                                      format, args);
+			MessageDialog md = new MessageDialog (parent, DialogFlags.Modal, msgtype, buttontype, format, args);
 			md.SetPosition(WindowPosition.CenterOnParent);    
-			ResponseType result = (ResponseType)md.Run ();
+			ResponseType result = (ResponseType)md.Run();
 			md.Destroy();
 			return result;
 		}
 
+		public static ResponseType Show(MessageType msgtype, ButtonsType buttontype, string format, params object[] args)
+		{
+			return Show(null, msgtype, buttontype, format, args);
+		}
+
+		public static ResponseType Show(MessageType msgtype, ButtonsType buttontype, string s)
+		{
+			return Show(null, msgtype, buttontype, s);
+		}
+
+		public static ResponseType Show(MessageType msgtype, string format, params object[] args)
+		{
+			return Show(null, msgtype, ButtonsType.Ok, format, args);
+		}
+
+		public static ResponseType Show(MessageType msgtype, string s)
+		{
+			return Show(null, msgtype, ButtonsType.Ok, s);
+		}
+
 		public static ResponseType Show(string format, params object[] args)
 		{
-			return Show(null, MessageType.Other, ButtonsType.Ok, format, args);
+			return Show(null, MessageType.Warning, ButtonsType.Ok, format, args);
 		}
 
 		public static ResponseType Show(string s)
 		{
-			return Show(null, MessageType.Other, ButtonsType.Ok, s);
+			return Show(null, MessageType.Warning, ButtonsType.Ok, s);
 		}
 	}
 }
