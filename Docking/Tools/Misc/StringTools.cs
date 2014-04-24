b7b2ac6f8138cc3711c810195c577716ea2801da
S.Lohse <steffen.lohse@neusoft.com>
2014-04-24 10:56:28 +0200

    added new bytes count to string function
    Change-Id: I389e195108285c209ec5a58b7eafe21d434a3ba3

--- Docking/Tools/Misc/StringTools.cs
+++ Docking/Tools/Misc/StringTools.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 
 namespace Docking.Tools
 {
@@ -29,6 +30,23 @@ namespace Docking.Tools
           if(s.Length<=0)
              return "";
           return Regex.Replace(s, "[\n\r]+", " ");
+      }
+
+      public static string ByteCountToString(ulong bytecount)
+      {
+         const ulong KILOBYTE = 1024;
+         const ulong MEGABYTE = 1024*1024;
+
+         if(bytecount>=1000000)
+            return String.Format(System.Globalization.CultureInfo.InvariantCulture, "{0:0.0} MB",
+                                 ((float) bytecount)/MEGABYTE);
+         else if(bytecount>=1000)
+            return String.Format(System.Globalization.CultureInfo.InvariantCulture, "{0:0.0} KB",
+                                 ((float) bytecount)/KILOBYTE);
+         else if(bytecount!=1)
+            return String.Format(System.Globalization.CultureInfo.InvariantCulture, "{0} Bytes", bytecount);
+         else
+            return "1 Byte";
       }
    }
 }
