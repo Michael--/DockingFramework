1a5042dcfa44b017f4fd13411559c8c79b785834
S.Lohse <steffen.lohse@neusoft.com>
2014-04-28 18:43:14 +0200

    use standard Windows icons in messageboxes
    Change-Id: Ic51977d34c338e33565586791f662d0651e439df

--- Docking/Framework/Components/MessageBox.cs
+++ Docking/Framework/Components/MessageBox.cs
@@ -1,17 +1,66 @@
 using System;
 using Gtk;
+using Docking.Tools;
+
 
 namespace Docking.Components
 {
     public class MessageBox
     {            
-      public static ComponentManager ComponentManager { get; set; } // gets assigned from inside the constructor MainWindow::MainWindow()
+       public static ComponentManager ComponentManager { get; private set; }
+
+       private static Gdk.Pixbuf PIXBUF_INFO;
+       private static Gdk.Pixbuf PIXBUF_WARNING;
+       private static Gdk.Pixbuf PIXBUF_QUESTION;
+       private static Gdk.Pixbuf PIXBUF_ERROR;
+
+       private static bool IsWindows()
+       {
+         switch(Environment.OSVersion.Platform)
+         {
+            case PlatformID.Win32S:       return true;
+            case PlatformID.Win32Windows: return true;
+            case PlatformID.Win32NT:      return true;
+            case PlatformID.WinCE:        return true;
+            case PlatformID.Unix:         return false;
+            case PlatformID.Xbox:         return false; // or should we return true here better???
+            case PlatformID.MacOSX:       return false;
+            default:                      return false;
+          }
+       }
+
+       public static void Init(ComponentManager cm)
+       {
+          ComponentManager = cm;
+
+          if(IsWindows())
+          {
+             PIXBUF_INFO     = SystemDrawing_vs_GTK_Conversion.Bitmap2Pixbuf(System.Drawing.SystemIcons.Information);
+             PIXBUF_WARNING  = SystemDrawing_vs_GTK_Conversion.Bitmap2Pixbuf(System.Drawing.SystemIcons.Warning);
+             PIXBUF_QUESTION = SystemDrawing_vs_GTK_Conversion.Bitmap2Pixbuf(System.Drawing.SystemIcons.Question);
+             PIXBUF_ERROR    = SystemDrawing_vs_GTK_Conversion.Bitmap2Pixbuf(System.Drawing.SystemIcons.Error);
+          }
+       }
 
 		public static ResponseType Show(Window parent, MessageType msgtype, ButtonsType buttontype, string format, params object[] args)
 		{
          if(parent==null && ComponentManager!=null)
             parent = ComponentManager;
 			MessageDialog md = new MessageDialog(parent, DialogFlags.Modal, msgtype, buttontype, format, args);
+
+         if(IsWindows()) // replace Gtk's private icons by Windows standard icons
+         {
+            switch(msgtype)
+            {
+            case MessageType.Info:     ((Gtk.Image)md.Image).Pixbuf = PIXBUF_INFO;     break;
+            case MessageType.Warning:  ((Gtk.Image)md.Image).Pixbuf = PIXBUF_WARNING;  break;
+            case MessageType.Question: ((Gtk.Image)md.Image).Pixbuf = PIXBUF_QUESTION; break;
+            case MessageType.Error:    ((Gtk.Image)md.Image).Pixbuf = PIXBUF_ERROR;    break;
+            case MessageType.Other:    ((Gtk.Image)md.Image).Pixbuf = PIXBUF_INFO;     break;
+            default:                   ((Gtk.Image)md.Image).Pixbuf = PIXBUF_INFO;     break;
+            }
+         }
+
          md.SetPosition(parent==null ? WindowPosition.Center : WindowPosition.CenterOnParent);
          if(ComponentManager!=null)
          {
@@ -23,6 +72,7 @@ namespace Docking.Components
             md.Title = "";
             md.Icon  = null;
          }
+
 			ResponseType result = (ResponseType)md.Run();
 			md.Destroy();
 			return result;
---
+++ Docking/Tools/Graphics/SystemDrawing_vs_GTK_Conversion.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace Docking.Tools
+{
+   public class SystemDrawing_vs_GTK_Conversion
+   {
+      // TODO is there a less brute-force way?
+      public static Gdk.Pixbuf Bitmap2Pixbuf(System.Drawing.Bitmap bitmap)
+      {
+         MemoryStream ms = new MemoryStream();
+         bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+         ms.Seek(0, SeekOrigin.Begin);
+         return new Gdk.Pixbuf(ms);
+      }
+
+      public static Gdk.Pixbuf Bitmap2Pixbuf(System.Drawing.Icon icon)
+      {
+         return Bitmap2Pixbuf(icon.ToBitmap());
+      }
+
+   }
+}
