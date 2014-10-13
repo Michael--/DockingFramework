2a65a547d63f7d1a419cb472f9ffda04e1712591
S.Lohse <steffen.lohse@neusoft.com>
2014-10-13 14:23:27 +0200

    allow retrieval of a unique OS identifier string
    , for example
    - "windows"
    - "ubuntu"
    - "arch"
    etc.
    
    this is needed for cases when the exact OS needs to be known and not just "Windows" vs "UNIX"
    
    Change-Id: If6e39c5db4348e63ed0c895a2a346efcd5fe0d90

--- Docking/Framework/Tools/Platform.cs
+++ Docking/Framework/Tools/Platform.cs
@@ -24,48 +24,98 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Gdk;
 
+
 namespace Docking.Tools
 {
-	public static class Platform
-	{
-		static Platform ()
-		{
- 			IsWindows = System.IO.Path.DirectorySeparatorChar=='\\';
+   public static class Platform
+   {
+      static Platform()
+      {
+         IsWindows = System.IO.Path.DirectorySeparatorChar=='\\';
          IsUNIX    = System.Environment.OSVersion.Platform==PlatformID.Unix ||
                      System.Environment.OSVersion.Platform==PlatformID.MacOSX;
- 			IsMac     = IsUNIX && IsRunningOnMac();
-		}
-		
-		public static bool IsWindows { get; private set; }
-      public static bool IsUNIX    { get; private set; }
-		public static bool IsMac     { get; private set; }
-		
-		//From Managed.Windows.Forms/XplatUI
-		static bool IsRunningOnMac ()
-		{
-			IntPtr buf = IntPtr.Zero;
-			try {
-				buf = Marshal.AllocHGlobal (8192);
-				// This is a hacktastic way of getting sysname from uname ()
-				if (uname (buf) == 0) {
-					string os = Marshal.PtrToStringAnsi (buf);
-					if (os == "Darwin")
-						return true;
-				}
-			} catch {
-			} finally {
-				if (buf != IntPtr.Zero)
-					Marshal.FreeHGlobal (buf);
-			}
-			return false;
-		}
-		
-		[DllImport ("libc")]
-		static extern int uname (IntPtr buf);
-	}
+         IsMac     = IsUNIX && IsRunningOnMac();
+
+         OSIDString = ComputeOSIDString();
+      }
+
+      public static bool IsWindows    { get; private set; }
+      public static bool IsUNIX       { get; private set; }
+      public static bool IsMac        { get; private set; }
+
+      public static string OSIDString { get; private set; }
+
+      //From Managed.Windows.Forms/XplatUI
+      static bool IsRunningOnMac()
+      {
+         IntPtr buf = IntPtr.Zero;
+         try
+         {
+            buf = Marshal.AllocHGlobal(8192);
+            // This is a hacktastic way of getting sysname from uname ()
+            if(uname(buf) == 0)
+            {
+               string os = Marshal.PtrToStringAnsi(buf);
+               if(os == "Darwin")
+                  return true;
+            }
+         }
+         catch
+         {}
+         finally
+         {
+            if(buf != IntPtr.Zero)
+               Marshal.FreeHGlobal(buf);
+         }
+         return false;
+      }
+
+      [DllImport("libc")]
+      static extern int uname(IntPtr buf);
+
+      // Returns a unique string identifying the currently running OS.
+      // For UNIXes, the ID= entry from /etc/os-release is used for this purpose.
+      // For other OSes, where this file does [of course] not exist, the behaviour is made similar.
+      private static string ComputeOSIDString()
+      {
+         PlatformID id = System.Environment.OSVersion.Platform;
+         switch(id)
+         {
+         case System.PlatformID.Win32S: return "windows"; // maybe in future we need to be more specific here, like "windows_7" or something, but currently just "windows" suffices
+         case System.PlatformID.Win32Windows: return "windows"; // maybe in future we need to be more specific here, like "windows_7" or something, but currently just "windows" suffices
+         case System.PlatformID.Win32NT: return "windows"; // maybe in future we need to be more specific here, like "windows_7" or something, but currently just "windows" suffices
+
+         case System.PlatformID.WinCE: return "windows_ce";
+         case System.PlatformID.Xbox: return "xbox";
+
+         case System.PlatformID.Unix:         /*NOP*/ break;
+         case System.PlatformID.MacOSX:       /*NOP*/ break;
+         default:                             /*NOP*/ break;
+         }
+
+         if(!System.IO.File.Exists("/etc/os-release"))
+            return "unknown";
+
+         string[] osinfo = System.IO.File.ReadAllLines("/etc/os-release");
+         foreach(string s in osinfo)
+         {
+            string[] portions = s.Split('=');
+            if(portions.Length!=2)
+               continue;
+            string key = portions[0];
+            string val = portions[1];
+
+            if(key.ToLowerInvariant()=="id")
+               return val; // for example "ubuntu" on Ubuntu, or "arch" on ArchLinux
+         }
+
+         return "unknown";
+      }
+   }
 }
