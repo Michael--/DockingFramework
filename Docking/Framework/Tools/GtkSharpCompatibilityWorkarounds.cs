801aae02aff581ab4a8e0b7dee9f109bb4cb7433
S.Lohse <steffen.lohse@neusoft.com>
2015-01-08 12:34:45 +0100

    Map Style Font Editor: Tried to read kerning information, but crap wont work.
    Change-Id: Icf97138c6c8bb7bd004b16f4bac9e7e8c98f98db

--- Docking/Framework/Tools/GtkSharpCompatibilityWorkarounds.cs
+++ Docking/Framework/Tools/GtkSharpCompatibilityWorkarounds.cs
@@ -1,15 +1,15 @@
-// Ubuntu 12.04 LTS currently only has an outdated GTK# binary delivery,
-// lacking these functions. We inject them here as a workaround.
-// We hope that at some point of time in the future, Ubuntu will contain an updated GTK# version
-// which contains these functions.
-
-#if __MonoCS__ // http://stackoverflow.com/questions/329043/how-can-i-conditionally-compile-my-c-sharp-for-mono-vs-microsoft-net
-
 using System;
 using Cairo;
+using System.Runtime.InteropServices;
 
 namespace Docking // TODO use a different namespace
 {
+   // Ubuntu 12.04 LTS currently only has an outdated GTK# binary delivery,
+   // lacking these functions. We inject them here as a workaround.
+   // We hope that at some point of time in the future, Ubuntu will contain an updated GTK# version
+   // which contains these functions.
+   // Update: Ubuntu 14.04 LTS does. Question is: what about other Linuxes??? Does this workaround clash there???
+   #if __MonoCS__ // http://stackoverflow.com/questions/329043/how-can-i-conditionally-compile-my-c-sharp-for-mono-vs-microsoft-net
 	public static class GtkSharpCompatibilityWorkarounds
 	{
 		public static void SetSource(this Context context, Surface surface)
@@ -40,7 +40,14 @@ namespace Docking // TODO use a different namespace
          #pragma warning restore 618
 		}
 	}
-	
-}
+   #endif
 
-#endif
+   public static class AdditionalGdkWrappers
+   {
+      [DllImport("libgdk-win32-2.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
+      public static extern IntPtr gdk_win32_hdc_get(IntPtr drawable, IntPtr gc, int usage);
+
+      [DllImport("libgdk-win32-2.0-0.dll", CallingConvention = CallingConvention.Cdecl)]
+      public static extern void gdk_win32_hdc_release(IntPtr drawable, IntPtr gc, int usage);
+   }
+}
