cf578acbf5ae72a07cfef6be3fae48d750ceabf3
S.Lohse <steffen.lohse@neusoft.com>
2016-02-02 15:02:37 +0100

    added static DEFAULT_FONT property
    Change-Id: I0fd31ae0756207ca5f8bfc5ad7b00b03807288d4

--- Docking/Framework/Manager/ComponentManager.cs
+++ Docking/Framework/Manager/ComponentManager.cs
@@ -803,6 +803,52 @@ namespace Docking.Components
       public static bool     PowerDown                { get; private set; }
       public Localization    Localization             { get; private set; }
 
+      public static string DEFAULT_FONT
+      {
+         get
+         {
+            string osid = Docking.Tools.Platform.OSIDString.ToLowerInvariant();
+            if(Docking.Tools.Platform.IsWindows)
+            {
+               // "Microsoft YaHei" is a sans-serif CJK font, installed by default on Windows 7 or higher.
+               // It also is the default font for the user interface on Chinese Win7.
+               // The font file contains all 20,902 original CJK Unified Ideographs code points specified in Unicode,
+               // plus approximately 80 code points defined by the Standardization Administration of China.
+               // It supports GBK character set, with localized glyphs.
+               // https://en.wikipedia.org/wiki/Microsoft_YaHei
+               // https://www.microsoft.com/typography/Fonts/family.aspx?FID=350
+               // Therefore it is the best candidate to use here.
+               // Fortunately, this font also properly supports European characters like äöüß etc., so we can use the same for CHN and non-CHN :)
+               // (We may not rely on "Office XYZ installed" here etc. -
+               // If the Win7 standard fonts are not enough, TG will have to deliver its own, which we currently try to avoid.)
+               // https://en.wikipedia.org/wiki/List_of_typefaces_included_with_Microsoft_Windows
+               // https://en.wikipedia.org/wiki/List_of_CJK_fonts
+               // TODO later let the user choose the font + size
+               return "Microsoft YaHei";
+            }
+            else if(osid.Contains("ubuntu"))
+            {
+               // "Droid Sans" is a font coming by default with Ubuntu 14.04 LTS ("Trusty Tahr")
+               // http://packages.ubuntu.com/trusty/fonts/
+               // https://en.wikipedia.org/wiki/Droid_Sans
+               // TODO later let the user choose the font + size
+               return "Droid Sans Fallback";
+            }
+            else
+            {
+               // Sadly, not all Linuxes come with Chinese fonts preinstalled.
+               // "Noto" is a free font by Google aiming to provide ALL Unicode characters.
+               // http://en.wikipedia.org/wiki/Noto_Sans
+               // http://en.wikipedia.org/wiki/Noto_Sans_CJK
+               // https://www.google.com/get/noto/help/cjk
+               // To get it, install the package containing it, for example on ArchLinux, that is
+               //    sudo pacman -S noto-fonts-cjk
+               // TODO later let the user choose the font + size
+               return "Noto Sans CJK SC";
+            }
+         }
+      }
+
       #endregion
 
       #region OpenFile
