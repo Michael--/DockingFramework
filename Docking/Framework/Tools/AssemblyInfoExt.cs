a54d86433c32766f94dfbb863ee72b1a605e7731
S.Lohse <steffen.lohse@neusoft.com>
2016-03-30 15:23:07 +0200

    added AssemblyInfoExt
    Change-Id: I821f764c1a6ca3fe6c66d5e7fc1f6850cd1e2a1a

---
+++ Docking/Framework/Tools/AssemblyInfoExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using System.IO;
+
+
+namespace Docking.Tools
+{
+   public class AssemblyInfoExt
+   {
+      static Assembly assembly = Assembly.GetEntryAssembly();
+
+      public static string  Location            { get { return assembly.Location;               } }     
+      public static string  Name                { get { return assembly.GetName().Name;         } }
+      public static Version Version             { get { return assembly.GetName().Version;      } }
+      public static string  Directory           { get { return Path.GetDirectoryName(Location); } }
+
+      public static string  LocalSettingsFolder { get {
+         return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), Name);
+      }}
+   }
+}
