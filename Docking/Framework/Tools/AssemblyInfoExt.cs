802f5cf8832cf3e501e03e8e82a5020de8c86938
S.Lohse <steffen.lohse@neusoft.com>
2016-04-14 20:58:57 +0200

    Resources folder added
    Change-Id: I87ccd3c7c38e2f13702bcb8cc425a8a678ecfbf8

--- Docking/Framework/Tools/AssemblyInfoExt.cs
+++ Docking/Framework/Tools/AssemblyInfoExt.cs
@@ -9,13 +9,16 @@ namespace Docking.Tools
    {
       static Assembly assembly = Assembly.GetEntryAssembly();
 
-      public static string  Location            { get { return assembly.Location;               } }     
-      public static string  Name                { get { return assembly.GetName().Name;         } }
-      public static Version Version             { get { return assembly.GetName().Version;      } }
-      public static string  Directory           { get { return Path.GetDirectoryName(Location); } }
+      public static string  Location            { get { return assembly.Location;                    } }     
+      public static string  Name                { get { return assembly.GetName().Name;              } }
+      public static Version Version             { get { return assembly.GetName().Version;           } }
+      public static string  Directory           { get { return Path.GetDirectoryName(Location);      } }
+      public static string  ResourcesDirectory  { get { return Path.Combine(Directory, "Resources"); } }
 
       public static string  LocalSettingsFolder { get {
          return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), Name);
       }}
+
+      
    }
 }
