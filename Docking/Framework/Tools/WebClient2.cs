998773c5a1a93eea11b20eef8ac231841bb25bee
S.Lohse <steffen.lohse@neusoft.com>
2020-12-01 16:44:04 +0100

    polish https TLS 1.2 fix again
    Change-Id: Iecfaec0ac46a0213d9c45d63e55440a977373f30

--- Docking/Framework/Tools/WebClient2.cs
+++ Docking/Framework/Tools/WebClient2.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Security;
 using System.Security.Cryptography;
 using System.Net;
 
@@ -35,20 +33,21 @@ namespace Docking.Tools
 
       static WebClient2()
       {
-         // Enable TLS 1.1 and TLS 1.2 (which are NOT enabled by default in .NET 4.5 on Windows 10!).
+         // Enable TLS 1.2 (which is NOT enabled by default in .NET 4.5 on Windows 10!).
          // The default value there is:
          //    ServicePointManager.SecurityProtocol = Ssl3 | Tls;
-         // Without this enabling, you will get this exception from some https sites
-         // which require TLS 1.1 or TLS 1.2:
+         // Without this enabling, you will get this exception from some https sites which require TLS 1.2:
          //    System.Net.WebException: The request was aborted: Could not create SSL/TLS secure channel
-         // https://docs.microsoft.com/windows/win32/secauthn/protocols-in-tls-ssl--schannel-ssp-
-         // https://de.wikipedia.org/wiki/Transport_Layer_Security#Versionen
-         // Note that there is lots of wrong information in the internet regarding this topic.
+         // https://www.itsicherheit-online.com/news/ende-fuer-tls-1.0-und-tls-1.1-naht
+         // https://www.zdnet.de/88378482/microsoft-verschiebt-support-ende-fuer-tls-1-0-und-1-1/       
+         // Note that there is lots of wrong information in the internet regarding this topic...
          ServicePointManager.SecurityProtocol |= (
-            SecurityProtocolType.Ssl3   // just in case that the default in future lacks this
-          | SecurityProtocolType.Tls    // just in case that the default in future lacks this
-          | SecurityProtocolType.Tls11  // not really needed, is already superceded by TLS 1.2
-          | SecurityProtocolType.Tls12  // THESE are the important flags
+                 // https://en.wikipedia.org/wiki/Transport_Layer_Security#SSL_1.0,_2.0,_and_3.0
+                 SecurityProtocolType.Ssl3   // SSL 3   - DEPRECATED, UNSAFE. Was superceded by TLS 1.0. We only enable it here for compatibility with ancient sites which have not upgraded to TLS 1.2 yet.
+               | SecurityProtocolType.Tls    // TLS 1.0 - DEPRECATED, UNSAFE. We only enable it here for compatibility with ancient sites which have not upgraded to TLS 1.2 yet.
+               | SecurityProtocolType.Tls11  // TLS 1.1 - DEPRECATED, UNSAFE. We only enable it here for compatibility with ancient sites which have not upgraded to TLS 1.2 yet.
+               | SecurityProtocolType.Tls12  // TLS 1.2 - The current standard. Soon to be superceded by TLS 1.3.
+            // | SecurityProtocolType.Tls13  // TLS 1.3 - The future standard (2018). Not available in .NET 4.5. https://docs.microsoft.com/en-us/dotnet/framework/network-programming/tls
          );
       }
 
