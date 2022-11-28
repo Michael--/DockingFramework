554e2125be9451f502940b121bc0d0c19d697837
T.Krause <torben.krause@de.neusoft.com>
2022-11-28 16:38:58 +0100

    JobInformation clean up/members sorted
    Change-Id: If114cb998ef275dfea86678206c779cbf33224ce

--- Docking/Framework/Tools/TaskInformation.cs
+++ Docking/Framework/Tools/TaskInformation.cs
@@ -1,35 +1,42 @@
+
 using System;
 using System.Threading;
 
 namespace Docking.Tools
 {
-    public class TaskInformation : JobInfo
-    {
-        private TaskInformation (CancellationTokenSource token, String name, String description)
-            : base(name, description)
-        {
-            m_TokenSource = token;
-        }
-        
-        CancellationTokenSource m_TokenSource;
-        
-        public static TaskInformation Create(CancellationTokenSource token, String name, String description)
-        {
-            TaskInformation job = new TaskInformation(token, name, description);
-            JobInfo.AddJob(job);
-            return job;
-        }
-        
-        public override bool CancelationSupported
-        {
-            get { return m_TokenSource != null; }
-        }
-        
-        public override void Cancel()
-        {
-            if (m_TokenSource != null)
-                m_TokenSource.Cancel();
-        }
-    }
-}
+   public class TaskInformation : JobInfo
+   {
+      private readonly CancellationTokenSource m_TokenSource;
 
+      private TaskInformation(CancellationTokenSource token, String name, String description)
+         : base(name, description)
+      {
+         m_TokenSource = token;
+      }
+
+      public bool IsCancellationRequested
+      {
+         get { return m_TokenSource.IsCancellationRequested; }
+      }
+
+      public override bool CancelationSupported
+      {
+         get { return m_TokenSource != null; }
+      }
+
+      public static TaskInformation Create(CancellationTokenSource token, String name, String description)
+      {
+         TaskInformation job = new TaskInformation(token, name, description);
+         AddJob(job);
+         return job;
+      }
+
+      public override void Cancel()
+      {
+         if (m_TokenSource != null)
+         {
+            m_TokenSource.Cancel();
+         }
+      }
+   }
+}
